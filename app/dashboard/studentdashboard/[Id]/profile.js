753de516e1e12749d8074b70879b1379642d6df7
Shivank Sharma <sharmashivank905@gamil.com>
2024-04-10 01:20:12 +0530

    bckjbc

--- app/dashboard/studentdashboard/[Id]/profile.js
+++ app/dashboard/studentdashboard/[Id]/profile.js
@@ -10,7 +10,7 @@ const UserDetails = () => {
     const fetchUserData = async () => {
       try {
         const res = await axios.get(
-          'http://localhost:3000/api/studentdash/getdata'
+          'https://doubt-buster.vercel.app/api/studentdash/getdata'
         )
         setUserData(res.data.users) // Assuming users is the key containing user data
         setLoading(false)
--- pages/api/sendroomid/email.js
+++ pages/api/sendroomid/email.js
@@ -68,9 +68,9 @@ export default async function handler(req, res) {
       const token = jwt.sign(payload, process.env.JWT_SECRET, {
         expiresIn: '1h',
       })
-      // const url = `https://doubt-buster.vercel.app/call/${token}`;
+      const url = `https://doubt-buster.vercel.app/call/${token}`;
 
-      const url = `http://localhost:3000/call/${token}`;
+      // const url = `http://localhost:3000/call/${token}`;
       ans=url;
       try {
         // Send the email
