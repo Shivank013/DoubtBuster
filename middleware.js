6f0e624cecb6b988abfb44874fd1e72567719bf9
Shivank Sharma <sharmashivank905@gamil.com>
2024-04-10 22:01:08 +0530

    jycjcjgcc

--- middleware.js
+++ middleware.js
@@ -1,53 +1,53 @@
-import { NextResponse } from 'next/server'
-import { jwtDecode } from 'jwt-decode'
-// This function can be marked `async` if using `await` inside
-export function middleware(request) {
-  console.log('middleware executed')
+// import { NextResponse } from 'next/server'
+// import { jwtDecode } from 'jwt-decode'
+// // This function can be marked `async` if using `await` inside
+// export function middleware(request) {
+//   console.log('middleware executed')
 
-  const authToken = request.cookies.get('token')?.value
-  console.log(authToken, 'lsdkfm')
+//   const authToken = request.cookies.get('token')?.value
+//   console.log(authToken, 'lsdkfm')
 
-  const loggedinuserNotAccessPaths =
-    request.nextUrl.pathname === '/login'
-    // request.nextUrl.pathname === '/signup'
+//   const loggedinuserNotAccessPaths =
+//     request.nextUrl.pathname === '/login'
+//     // request.nextUrl.pathname === '/signup'
 
-  if (loggedinuserNotAccessPaths) {
-    if (authToken) {
-      const decoded = jwtDecode(authToken)
-      console.log(decoded)
-      if (decoded.role === 'Instructor') {
-        return NextResponse.redirect(
-          new URL('/dashboard/expertdashboard', request.url)
-        )
-      } else {
-        return NextResponse.redirect(
-          new URL(`/dashboard/studentdashboard/${decoded.id}`, request.url)
-        )
-      }
-      // Decode the JWT token
-      // const decodedToken = jwtdecode(authToken)
-      // Extract role from the decoded token
-      // const userRole = decodedToken.rolec
-      // console.log(decodedToken, 'whats your role')
-      //   return NextResponse.redirect(
-      //     new URL('/dashboard/expertdashboard', request.url)
-      //   )
-    }
-  } else {
-    if (!authToken) {
-      return NextResponse.redirect(new URL('/login', request.url))
-    }
-  }
+//   if (loggedinuserNotAccessPaths) {
+//     if (authToken) {
+//       const decoded = jwtDecode(authToken)
+//       console.log(decoded)
+//       if (decoded.role === 'Instructor') {
+//         return NextResponse.redirect(
+//           new URL('/dashboard/expertdashboard', request.url)
+//         )
+//       } else {
+//         return NextResponse.redirect(
+//           new URL(`/dashboard/studentdashboard/${decoded.id}`, request.url)
+//         )
+//       }
+//       // Decode the JWT token
+//       // const decodedToken = jwtdecode(authToken)
+//       // Extract role from the decoded token
+//       // const userRole = decodedToken.rolec
+//       // console.log(decodedToken, 'whats your role')
+//       //   return NextResponse.redirect(
+//       //     new URL('/dashboard/expertdashboard', request.url)
+//       //   )
+//     }
+//   } else {
+//     if (!authToken) {
+//       return NextResponse.redirect(new URL('/login', request.url))
+//     }
+//   }
 
-  //   return NextResponse.redirect(new URL('/home', request.url))
-}
+//   //   return NextResponse.redirect(new URL('/home', request.url))
+// }
 
-export const config = {
-  matcher: [
-    '/dashboard/expertdashboard/:path*',
+// export const config = {
+//   matcher: [
+//     '/dashboard/expertdashboard/:path*',
 
-    '/login',
-    '/signup',
-    '/dashboard/studentdashboard/:path*',
-  ],
-}
+//     '/login',
+//     '/signup',
+//     '/dashboard/studentdashboard/:path*',
+//   ],
+// }
