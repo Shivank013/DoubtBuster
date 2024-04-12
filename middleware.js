d2eb6c39f957533c31246cad1564d83a4ffb18ca
Shivank Sharma <sharmashivank905@gamil.com>
2024-04-12 10:25:58 +0530

    khvsvhv

--- middleware.js
+++ middleware.js
@@ -1,58 +1,42 @@
-import { NextResponse } from 'next/server'
-import { jwtDecode } from 'jwt-decode'
-// This function can be marked async if using await inside
+import { NextResponse } from 'next/server';
+import { jwtDecode } from 'jwt-decode';
+
 export function middleware(request) {
-  console.log('middleware executed')
+  console.log('middleware executed');
 
-  const authToken = request.cookies.get('token')?.value
-  console.log(authToken, 'lsdkfm')
+  const authToken = request.cookies.get('token')?.value;
 
-  const loggedinuserNotAccessPaths =
-    request.nextUrl.pathname === '/login'||
-    request.nextUrl.pathname === '/signup'
+  if (authToken) {
+    const decoded = jwtDecode(authToken);
+    console.log(decoded);
 
-  if (loggedinuserNotAccessPaths && authToken) {
-   
-      const decoded = jwtDecode(authToken)
-      console.log(decoded)
-      if (decoded.role === 'Instructor') {
-        return NextResponse.redirect(
-          new URL('/dashboard/expertdashboard', request.url)
-        )
-      } else {
-        return NextResponse.redirect(
-          new URL(`/dashboard/studentdashboard/${decoded.id}`, request.url)
-        )
-      }
-      // }
-      // else{
-        
-      //   if (!authToken && request.nextUrl.pathname === '/login'){
-      //     return NextResponse.redirect(
-      //       new URL('/login', request.url)
-      //     )
-      //   } else {
-      //     return NextResponse.redirect(
-      //       new URL('/signup', request.url)
-      //     )
-      //   }
-      // }
-    
+    if (decoded.role === 'Instructor') {
+      return NextResponse.redirect(
+        new URL('/dashboard/expertdashboard', request.url)
+      );
+    } else {
+      return NextResponse.redirect(
+        new URL(`/dashboard/studentdashboard/${decoded.id}`, request.url)
+      );
+    }
   } else {
-    if (!authToken) {
-      return NextResponse.redirect(new URL('/login', request.url))
+    // If no token present, allow access to login and signup pages
+    if (
+      request.nextUrl.pathname !== '/login' &&
+      request.nextUrl.pathname !== '/signup'
+    ) {
+      return NextResponse.redirect(new URL('/login', request.url));
     }
   }
-  return NextResponse.next()
-  //   return NextResponse.redirect(new URL('/home', request.url))
+
+  return NextResponse.next();
 }
 
 export const config = {
   matcher: [
     '/dashboard/expertdashboard/:path*',
-    // '/',
     '/login',
     '/signup',
     '/dashboard/studentdashboard/:path*',
   ],
-}
+};
