59294580fcf8f636b04c44ce77149697f344bef7
Shivank Sharma <shivanks224@gmail.com>
2024-08-05 07:27:45 +0530

    Update peer.js

--- services/peer.js
+++ services/peer.js
@@ -19,7 +19,7 @@ class PeerService {
             //     credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
             //     username: '28224511:1379330808'
             // },
-            {
+            // {
             //     url: 'turn:192.158.29.39:3478?transport=tcp',
             //     credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
             //     username: '28224511:1379330808'
